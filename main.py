1f1c930e1801123c634d5c0041d193caf88a174a
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2021-10-19 15:39:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,30 +9,183 @@ def clear():
         _ = os.system('cls')
 
 def typing_tips():
+    clear()
     print("====================== WELCOME TO THE TYPEWRITER-TIPS ======================")
     print("1) First sit with comfarat and place your keyboard at right place.")
     print("2) Use your all fingers for typing with the thumb used for space bar.")
     print("3) Explore the keys on the keyboard with the use of the fingers.")
     print("4) Learn the new shortcut keys using the keyboard")
     print("5) Avoid to watch keyboard while typing and try keep your eyes on screen.")
-    decision = input("[ If you want to go back yes/no :- y/n ]")
-    if decision == "y":
+    decision = input("[ If you want to go back yes/no :- Y/n ]")
+    if decision == "Y":
         repeat()
     else:
         exit()
     
 def typing_practice():
-    decision = input("[Want to try short, long paragraph:- 's/l' ] ")
+    decision = input("[Want to try :- 'Y/n' ] ")
 
-    if decision == "s":
+    if decision == "Y":
         file = open('paragraph.txt', 'r')
         Lines = file.readlines()
-        print(random.choice(Lines))
+        st = random.choice(Lines)
+        if st[-1] !=".":
+            st += "."
 
+        print(st)
+        word_count = 0
+        l = []
+        word = ""
+        for i in st:
+            if i == " " or i == ".":
+                l.append(word)
+                word = ""
+                word_count += 1
+            else:
+                word += i
+                continue
+
+        for i in range(l.count(" ")):
+            l.remove(" ")
+
+        print("NOTE :- Try to write the equal words in corresponds to the Paragraph", end=",")
+        print("The Accuracy may vary a lot")
+        print("Enter your words:- ")
+        start = time.time()
+        your_st = input()
+        end = time.time()
+
+        m = []
+        for i in your_st:
+            if i == " " or i == ".":
+                m.append(word)
+                word = ""
+                word_count += 1
+            else:
+                word += i
+                continue
+
+        for i in range(m.count(" ")):
+            m.remove(" ")
+
+        correct = 0
+
+        if len(l) > len(m):
+            for i in range(len(l) - len(m)):
+                m.append(" ")
+
+        if len(l) < len(m):
+            for i in range(len(m) - len(l)):
+                m.pop()
+
+        for i in range(len(l)):
+            if l[i] == m[i]:
+                correct += 1
+
+        accuracy = correct/len(m)
+        print("Time :- " + str(round(end - start, 2)) + "s")
+        print("Accuracy :- " + str(round(accuracy, 2) * 100 ) + "%")
+
+        dec = input("[Want to try more :- Y/n ]")
+        if dec == "Y":
+            repeat()
+        else:
+            exit()
+
+
+    if decision == "n":
+        print("OK !")
+        dec = input("[Want to try more :- Y/n ]")
+        if dec == "Y":
+            repeat()
+        else:
+            exit()
 
 def typing_test():
-    print("Hello world")
-    pass
+    clear()
+    decision = input("[Want to try :- 'Y/n' ] ")
+
+    if decision == "Y":
+        file = open('paragraph.txt', 'r')
+        Lines = file.readlines()
+        st = random.choice(Lines)
+        if st[-1] !=".":
+            st += "."
+
+        print("\n")
+        print("Your Paragraph is here :-")
+
+        print(st)
+        word_count = 0
+        l = []
+        word = ""
+        for i in st:
+            if i == " " or i == ".":
+                l.append(word)
+                word = ""
+                word_count += 1
+            else:
+                word += i
+                continue
+
+        for i in range(l.count(" ")):
+            l.remove(" ")
+
+        print("NOTE :- Try to write the equal words in corresponds to the Paragraph", end=",")
+        print("The Accuracy may vary a lot")
+        print("Enter your words:- ")
+        start = time.time()
+        your_st = input()
+        end = time.time()
+
+        m = []
+        for i in your_st:
+            if i == " " or i == ".":
+                m.append(word)
+                word = ""
+                word_count += 1
+            else:
+                word += i
+                continue
+
+        for i in range(m.count(" ")):
+            m.remove(" ")
+
+        correct = 0
+
+        if len(l) > len(m):
+            for i in range(len(l) - len(m)):
+                m.append(" ")
+
+        if len(l) < len(m):
+            for i in range(len(m) - len(l)):
+                m.pop()
+
+        for i in range(len(l)):
+            if l[i] == m[i]:
+                correct += 1
+
+        accuracy = correct/len(m)
+        Time = end - start
+        print("Time :- " + str(round(end - start, 2)) + "s")
+        print("Accuracy :- " + str(round(accuracy, 2) * 100 ) + "%")
+        print("Words per minute :- " + str(round((len(m)/Time) * 60, 2)))
+
+        dec = input("[Want to try more :- Y/n ]")
+        if dec == "Y":
+            repeat()
+        else:
+            exit()
+
+
+    if decision == "n":
+        print("OK !")
+        dec = input("[Want to try more :- Y/n ]")
+        if dec == "Y":
+            repeat()
+        else:
+            exit()
+
 
 
 def repeat():
@@ -87,7 +240,11 @@ if __name__ == '__main__':
         typing_test()# going to typing test
 
     if choice == "4":
-        exit() # Exit
+        clear()
+        decision = input("[Really Want to exit :- 'Y/n']")
+        if decision == "Y":
+            exit() # Exit
+        if decision == "n":
+            repeat() # repeat
 
-
-        
+# End of the code of typing test.
